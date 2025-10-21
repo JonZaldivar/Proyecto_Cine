0c2347a6c148dcd1fc8d45c93084c6cb46011f0d
Jon Zaldivar <jon.zaldivar@opendeusto.es>
2025-10-21 11:25:48 +0200

    prueba 3

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(12);
+		
+		System.out.println("probando");
+		
+		System.out.println("pruebilla 3 ");
 
-	}
-
-}
+}}
