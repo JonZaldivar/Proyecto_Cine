371ba0cbae7b2d9f0db904ff1842bc80cf87b840
Jon Castaño <jon.castano@opendeusto>
2025-10-22 09:50:45 +0200

    pelicula

--- src/proyectoCine/domain/Pelicula.java
+++ src/proyectoCine/domain/Pelicula.java
@@ -4,11 +4,18 @@ import java.util.List;
 
 public class Pelicula {
 	
+	public enum Genero {
+	    ACCION, AVENTURAS, COMEDIA, DRAMA, FANTASIA,
+	    HISTORICO, HORROR, MUSICAL, ROMANCE, CIENCIA_FICCION,
+	    SUSPENSE, TERROR, WESTERN
+	}
+
 	private int id;
 	private String titulo;
 	private String director;
 	private int duracion;
 	private List<Actor> actores;
+	private Genero genero;
 	
 
 }
