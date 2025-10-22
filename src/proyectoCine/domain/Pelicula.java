7ae56b2630793dd10d1f2078236123e62efbcbc9
AlejandroGarcia-p <alejandro.garcia.p@opendeusto.es>
2025-10-22 10:05:59 +0200

    pruebilla

--- src/proyectoCine/domain/Pelicula.java
+++ src/proyectoCine/domain/Pelicula.java
@@ -1,6 +1,7 @@
 package proyectoCine.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Pelicula {
 	
@@ -16,6 +17,41 @@ public class Pelicula {
 	private int duracion;
 	private List<Actor> actores;
 	private Genero genero;
+	
+	public Pelicula(int id, String titulo, String director, int duracion, List<Actor> actores, Genero genero) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.director = director;
+		this.duracion = duracion;
+		this.actores = actores;
+		this.genero = genero;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pelicula other = (Pelicula) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Pelicula [id=" + id + ", titulo=" + titulo + ", director=" + director + ", duracion=" + duracion
+				+ ", actores=" + actores + ", genero=" + genero + "]";
+	}
+	
+	
 	
 
 }
