6b52dea7a5fbd9364377a0714d3d1b18881658ec
AlejandroGarcia-p <alejandro.garcia.p@opendeusto.es>
2025-10-22 07:48:43 +0200

    pruebilla4

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,10 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(12);
 		
-		System.out.println("probando");
-		
-		System.out.println("pruebilla 3 ");
 
 }}
