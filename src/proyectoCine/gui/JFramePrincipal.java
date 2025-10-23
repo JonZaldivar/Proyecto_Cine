6abda3cab7568a31605f6acdba4784c798bc8499
AlejandroGarcia-p <alejandro.garcia.p@opendeusto.es>
2025-10-23 12:36:13 +0200

    filtro para la tablaPeliculas por titulo

--- src/proyectoCine/gui/JFramePrincipal.java
+++ src/proyectoCine/gui/JFramePrincipal.java
@@ -12,6 +12,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.WindowConstants;
 import javax.swing.border.TitledBorder;
@@ -26,6 +27,7 @@ public class JFramePrincipal extends JFrame {
 	private JTable tablaPeliculas;
 	private DefaultTableModel modeloDatosPeliculas;
 	private JScrollPane scrollPeliculas;
+	private String filtroActual = "";
 	
 	
 	public JFramePrincipal(List<Pelicula> peliculas) {
@@ -46,8 +48,7 @@ public class JFramePrincipal extends JFrame {
 		
 		this.getContentPane().add(panelTablaPeliculas,BorderLayout.CENTER);
 		
-		
-		
+			
 		
 		JPanel panelCabecera = new JPanel();
 		panelCabecera.setBackground(Color.BLUE);
@@ -58,6 +59,32 @@ public class JFramePrincipal extends JFrame {
 		
 		
 		this.getContentPane().add(panelCabecera,BorderLayout.NORTH);
+		
+		JPanel panelFiltro = new JPanel();
+		JTextField filtro = new JTextField();
+		filtro.setColumns(20);
+		panelFiltro.add(new JLabel("FILTRAR POR TÍTULO: "));
+		panelFiltro.add(filtro);
+		panelFiltro.setBackground(Color.green);
+		
+		filtro.getDocument().addDocumentListener(new javax.swing.event.DocumentListener() {
+		    @Override
+		    public void insertUpdate(javax.swing.event.DocumentEvent e) { actualizarFiltro(); }
+		    @Override
+		    public void removeUpdate(javax.swing.event.DocumentEvent e) { actualizarFiltro(); }
+		    @Override
+		    public void changedUpdate(javax.swing.event.DocumentEvent e) { actualizarFiltro(); }
+
+		    private void actualizarFiltro() {
+		        filtroActual = filtro.getText();
+		        filtrarPeliculas(filtroActual); // filtra la tabla
+		        tablaPeliculas.repaint();       // fuerza al JTable a repintar con el nuevo renderer
+		    }
+		});
+		
+		
+		
+		this.getContentPane().add(panelFiltro,BorderLayout.SOUTH);
 		
 		
 		this.setTitle("Ventana principal de Cartelera");		
@@ -95,6 +122,22 @@ public class JFramePrincipal extends JFrame {
 					pelicula.getId(),pelicula.getTitulo(),pelicula.getDirector()
 					,pelicula.getGenero()
 			});
+		}
+		
+	}
+	
+	private void filtrarPeliculas(String texto) {
+		
+		texto = texto.toLowerCase();
+		this.modeloDatosPeliculas.setRowCount(0);
+		
+		for(Pelicula pelicula : this.peliculas) {
+			if(pelicula.getTitulo().toLowerCase().contains(texto)) {
+				this.modeloDatosPeliculas.addRow(new Object[] {
+						pelicula.getId(),pelicula.getTitulo(),
+						pelicula.getDirector(),pelicula.getGenero()
+				});
+			}
 		}
 		
 	}
