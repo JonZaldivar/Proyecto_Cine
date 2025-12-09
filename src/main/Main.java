2f126da748aa7111bbddf31f5b2702c5cddcdabd
AlejandroGarcia-p <alejandro.garcia.p@opendeusto.es>
2025-12-09 20:58:06 +0100

    bases

--- src/main/Main.java
+++ src/main/Main.java
@@ -23,7 +23,7 @@ public class Main {
     public static void main(String[] args) {
 
     	CineGestorBD gestor = new CineGestorBD();
-    	//gestor.crearTablas();
+    	gestor.crearTablas();
         gestor.cargarPeliculas();
         gestor.cargarActores();
         gestor.cargarPeliculaActores();
